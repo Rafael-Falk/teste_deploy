2f6ae8809d3c281506f69d48c3315aa617cf0652
Rafael-Falk <rafelbiondo@gmail.com>
2024-12-10 19:47:37 -0300

    deploy

--- app_exemplo/settings.py
+++ app_exemplo/settings.py
@@ -93,15 +93,10 @@ WSGI_APPLICATION = 'app_exemplo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Usando o backend MySQL
-        'NAME': 'produtos',                  # Substitua pelo nome do seu banco de dados
-        'USER': 'root',                       # Usuário do MySQL
-        'PASSWORD': 'senacrs',                       # Senha do MySQL (adicione a senha se existir)
-        'HOST': '127.0.0.1',                  # Endereço do servidor MySQL
-        'PORT': '3306',                       # Porta padrão do MySQL
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
